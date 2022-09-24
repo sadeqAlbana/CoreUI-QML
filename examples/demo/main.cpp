07238f8e184c94105b4d92f5dea9c47c8bf5d6e6
Sadeq Albana <sadeq.albana@snono-systems.com>
2022-09-24 12:26:35 +0300

    added icons

--- examples/demo/main.cpp
+++ examples/demo/main.cpp
@@ -3,6 +3,8 @@
 #include <QDebug>
 #include <QIcon>
 #include <QtQml/qqmlextensionplugin.h>
+#include <QFile>
+#include <QDir>
 Q_IMPORT_QML_PLUGIN(CoreUIPlugin)
 
 int main(int argc, char *argv[])
@@ -11,12 +13,15 @@ int main(int argc, char *argv[])
     QIcon::setThemeName("CoreUI");
 
     QQmlApplicationEngine engine;
-    qDebug()<<engine.importPathList();
     engine.addImportPath(QStringLiteral(":/"));
     engine.addImportPath(QStringLiteral(":/qml"));
 
     const QUrl url(u"qrc:/Demo/main.qml"_qs);
     qDebug()<<engine.importPathList();
+
+    qDebug()<<"exists: " <<::QFile::exists(":/icons/CoreUI/free/cil-speedometer.svg");
+    qDebug()<<"dir exists: " <<QDir(":/icons").exists();
+
 
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
